f1e1a93126846ca4b8d3a843434a6110b9c6c075
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-11-15 10:54:07 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 
 export default function App() {
@@ -9,7 +10,6 @@ export default function App() {
         <h1>⚡ X-treme Weather</h1>
       </header>
       <Weather />
-
       <footer>
         Coded by Cheavon Pottinger and is on
         <a href="https://github.com/Trinity737/X-treme-App"> Github</a>
--- src/Weather.js
+++ src/Weather.js
@@ -3,11 +3,29 @@ import React from "react";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="col-6">Toronto, Ontario</div>
+      <h1>Toronto, Ontario</h1>
       <ul>
-        <li className="description">Sunny</li>
         <li className="date">Wednesday 12:00pm</li>
+        <li className="description">Sunny</li>
       </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://j.theweathernetwork.com/wx_icons/v1/2.png"
+            alt="sunny"
+            width="60"
+            rel="noreferrer"
+          />
+          24°
+          <a href="/">C</a> / <a href="/">F</a>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: 30 %</li>
+            <li>Wind: 5 Km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
