e9843a52dd39f251658abb04b4fa96772ed91c40
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-11-15 12:37:44 -0500

    added Forecast component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Weather from "./Weather";
+import Forecast from "./Forecast";
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 
@@ -9,10 +10,27 @@ export default function App() {
       <header className="App-header">
         <h1>⚡ X-treme Weather</h1>
       </header>
+      <form>
+        <div className="row">
+          <div className="col-8">
+            <input
+              type="search"
+              placeholder="Enter a City"
+              className="form-control"
+            />
+          </div>
+          <div className="col-4">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
       <Weather />
+      <Forecast />
       <footer>
-        Coded by Cheavon Pottinger and is on
-        <a href="https://github.com/Trinity737/X-treme-App"> Github</a>
+        <small>
+          This project was coded by Cheavon Pottinger and is open-sourced on
+          <a href="https://github.com/Trinity737/X-treme-App"> Github</a>
+        </small>
       </footer>
     </div>
   );
